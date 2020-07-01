af1683369de627e16651c775a78a435539e15bb4
bainss <surjit.bains@gmail.com>
2020-07-01 22:34:47 +0100

    Update istio-operator.tf

--- istio-operator.tf
+++ istio-operator.tf
@@ -7,30 +7,27 @@ locals {
   )
 }
 
-data "kubectl_file_documents" "istio_operator" {
+data "kubectl_path_documents" "istio_operator_crds" {
   count   = local.istio_operator["enabled"] ? 1 : 0
-  content = file("./templates/istio_operator.yaml")
+  pattern = "./templates/istio-operator/crds/*.yaml"
+}
+
+data "kubectl_path_documents" "istio_operator" {
+  count   = local.istio_operator["enabled"] ? 1 : 0
+  pattern = "./templates/istio-operator/*.yaml"
+}
+
+resource "kubectl_manifest" "istio_operator_crds" {
+  count     = local.istio_operator["enabled"] ? length(data.kubectl_path_documents.istio_operator_crds[0].documents) : 0
+  yaml_body = element(data.kubectl_path_documents.istio_operator_crds[0].documents, count.index)
 }
 
 resource "kubectl_manifest" "istio_operator" {
-  count     = local.istio_operator["enabled"] ? length(data.kubectl_file_documents.istio_operator[0].documents) : 0
-  yaml_body = element(data.kubectl_file_documents.istio_operator[0].documents, count.index)
-}
-locals {
-  istio_operator = merge(
-    {
-      enabled = false
-    },
-    var.istio_operator
-  )
-}
+  count     = local.istio_operator["enabled"] ? length(data.kubectl_path_documents.istio_operator[0].documents) : 0
+  yaml_body = element(data.kubectl_path_documents.istio_operator[0].documents, count.index)
 
-data "kubectl_file_documents" "istio_operator" {
-  count   = local.istio_operator["enabled"] ? 1 : 0
-  content = file("./templates/istio_operator.yaml")
+  depends_on = [
+    kubectl_manifest.istio_operator_crds
+  ]
 }
-
-resource "kubectl_manifest" "istio_operator" {
-  count     = local.istio_operator["enabled"] ? length(data.kubectl_file_documents.istio_operator[0].documents) : 0
-  yaml_body = element(data.kubectl_file_documents.istio_operator[0].documents, count.index)
 }
